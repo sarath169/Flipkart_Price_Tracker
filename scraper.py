8ba0d036686421a167f9b6fef4ec01e4f5ee398a
sarath169 <rayala@iiitkalyani.ac.in>
2021-02-09 17:30:22 +0530

    changed the names and removed the static files

--- scraper.py
+++ scraper.py
@@ -36,13 +36,13 @@ def scraper(link):
     return laptops
 def inserting(laptops):
     # try:
-    sql = mysql.connector.connect(
+    mysql = mysql.connector.connect(
     host="localhost",
     user="root",
     password="Mindfire#12",
     database="flipkart_laptops"
     )
-    cursor=sql.cursor()
+    cursor=mysql.cursor()
     n=len(laptops)
     for i in range(n):
         logging.info('executed times',i)
@@ -59,68 +59,27 @@ def inserting(laptops):
         if pid:
             logging.info("in the if statement",pid)
             cursor.execute(query,(list_price,date,pid,))
-            sql.commit()
+            mysql.commit()
         else:
             logging.info("else block",i)
             q1="insert into products (product_name) values (%s)"
             cursor.execute(q1,(name,))
-            sql.commit()
+            mysql.commit()
             cursor.execute("SELECT product_id FROM products WHERE product_name = %s",(name,))
             res=cursor.fetchone()
             pid=res[0]
             cursor.execute(query, (list_price,date,pid,))
-            sql.commit()
-    sql.close()
-    # except mysql.connector.Error as error:
-    #     print("Failed to insert record into Laptop table, Error is: {}".format(error))
-
-        # query='''IF EXISTS (cursor.execute("SELECT * FROM products WHERE product_name = %s",(name,)))
-        #         BEGIN
-        #         cursor.execute("insert into price (listed_price, date_time, product_id) values (%s, %s, %s)",
-        #         (list_price,date,pid,)    #         );
-        #         END
-        #         ELSE
-        #         BEGIN
-        #             cursor.execute("insert into products (product_name) values (%s)",
-        #             (name,)
-        #             );
-        #             cursor.execute("insert into price (listed_price, date_time, product_id) values (%d, %s, %d)",
-        #             (laptops[i][2],"laptops[i][4]",(select product_id from products where product_name=laptops[i][0]),)
-        #             )
-        #         END'''
-# def log_to_xlsx(laptops):
-#     #
-#     log_records=[]
-#     for i in laptops:
-#         log_records.append(i)
-#         logging.info(i)
-#     wb_log=Workbook()
-#     ws_log=wb_log.active
-#     ws_log['A1']='Log_Info'
-#     for logs in log_records:
-#         ws_log.append(logs)
-#     wb_log.save("Logging Report.xlsx")
-#     return 0
-
-# def data_to_xlsx(laptops):
-#     wb=Workbook()
-#     # grab the active worksheet
-#     ws = wb.active
-#     # Data can be assigned directly to cells
-#     ws['A1'] = 'Name'
-#     ws['B1'] = 'Ratings'
-#     ws['C1'] = 'Listing Price'
-#     ws['D1'] = 'Actual Price'
-#     ws['E1'] = 'Date and Time'
-#     # Rows can also be appended
-#     for i in laptops:
-#         ws.append(i)
-#     # Python types will automatically be converted
-#     # Save the file
-#     d=datetime.datetime.now()
-#     wb.save("laptops_list_"+str(d)+".xlsx")
-#     return 0
+            mysql.commit()
+    mysql.close()
+'''def plot():
+    mysql = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Mindfire#12",
+    database="flipkart_laptops"
+    )
+    cursor=mysql.cursor()
+    
+'''
 result=scraper(FLIPKART_LAPTOPS)
 inserting(result)
-
-# data_to_xlsx(result)
