30e53ac3c5d2abe5e69056c04da949d15855ed51
sarath169 <rayala@iiitkalyani.ac.in>
2021-02-18 19:20:21 +0530

    modified alert.py

--- alert.py
+++ alert.py
@@ -6,8 +6,7 @@ import smtplib, ssl
 import dotenv
 import logging
 
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
+
 
 def connection():
     dotenv.load_dotenv()
@@ -42,46 +41,20 @@ def alert_mail(sql):
             else:
                 sender_email =  os.getenv('sender_email')
                 receiver_email = i[0]
-                password = os.getenv('email_pwd')
+                pwd = os.getenv('email_pwd')
                 port=465
-                message = MIMEMultipart("alternative")
-                message["Subject"] = "Price Drop Alert"
-                message["From"] = sender_email
-                message["To"] = receiver_email
+                smtp_server = "smtp.gmail.com"
+                sender_email = sender_email  # Enter your address
+                receiver_email = receiver_email # Enter receiver address
+                password = pwd
+                message = """\
+                Subject: "Price Drop alert"
 
-                # Create the plain-text and HTML version of your message
-                text = """\
-                Hi,
-                How are you?
-                Real Python has many great tutorials:
-                www.realpython.com"""
-                html = """\
-                <html>
-                  <body>
-                    <p>Hi,<br>
-                       Hope you are doing well<br>
-                       The product which you are intrested is below the threshold.
-                    </p>
-                  </body>
-                </html>
-                """
-
-                # Turn these into plain/html MIMEText objects
-                part1 = MIMEText(text, "plain")
-                part2 = MIMEText(html, "html")
-
-                # Add HTML/plain-text parts to MIMEMultipart message
-                # The email client will try to render the last part first
-                message.attach(part1)
-                message.attach(part2)
-
-                # Create secure connection with server and send email
+                there is a price drop in the product your are intrested in."""
                 context = ssl.create_default_context()
-                with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+                with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
                     server.login(sender_email, password)
-                    server.sendmail(
-                        sender_email, receiver_email, message.as_string()
-                    )
+                    server.sendmail(sender_email, receiver_email, message)
 
 def main():
 
